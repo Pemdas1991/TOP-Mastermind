2a8bf39d1ebf77a2c4bc7149472c6927fd55fd65
Pemdas1991 <Pemdas1991@gmail.com>
2022-12-08 16:59:36 -0700

    create game and classe. Lay out psudo-code for game

---
+++ c_player.rb
@@ -0,0 +1,7 @@
+class CPlayer
+
+def generate_code
+
+
+
+end
---
+++ game.rb
@@ -0,0 +1,24 @@
+class Game
+
+	def initialize
+		@round = 0
+		self.play
+	end
+
+	def play
+		win = false
+		while win == false do
+
+			round =+ 1
+			#c_player create pattern
+			#player guess pattern
+			#check pattern
+				#is it perfect
+				#are colors in the right place
+				#Are the colors correct
+			#if pattern is correct win = true
+
+	end
+
+
+end
