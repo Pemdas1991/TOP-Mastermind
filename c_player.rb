28cefd7758820fca34b0c3df7e8b9312bf4cd33c
Pemdas1991 <Pemdas1991@gmail.com>
2022-12-08 18:28:54 -0700

    update cPlayer class to generate random color array

--- c_player.rb
+++ c_player.rb
@@ -1,7 +1,34 @@
+# frozen_string_literal: true
+
 class CPlayer
+  def initialize
+    @prng = Random.new
+    @rand_array = Array.new(4, 0)
+  end
 
-def generate_code
+  def generate_code
+    @rand_array.each_index { |i| @rand_array[i] = @prng.rand(6) + 1 }
+  end
 
-
-
+  def convert_to_color
+    @rand_array.each_index do |i|
+      # colors: red, orange,  yellow, green, blue, purple
+      @rand_array[i] =
+        case @rand_array[i]
+        when 6
+          'R'
+        when 5
+          'O'
+        when 4
+          'Y'
+        when 3
+          'G'
+        when 2
+          'B'
+        else
+          'P'
+        end
+    end
+    @rand_array
+  end
 end
--- game.rb
+++ game.rb
@@ -1,24 +1,22 @@
+# frozen_string_literal: true
+
 class Game
+  def initialize
+    @round = 0
+    play
+  end
 
-	def initialize
-		@round = 0
-		self.play
-	end
+  def play
+    win = false
+    while win == false
 
-	def play
-		win = false
-		while win == false do
-
-			round =+ 1
-			#c_player create pattern
-			#player guess pattern
-			#check pattern
-				#is it perfect
-				#are colors in the right place
-				#Are the colors correct
-			#if pattern is correct win = true
-
-	end
-
-
+      @round = + 1
+      # c_player create pattern
+      # player guess pattern
+      # check pattern
+      	# is it perfect - if yes win = true
+      	# are colors in the right place
+      	# Are the colors correct
+    end
+  end
 end
